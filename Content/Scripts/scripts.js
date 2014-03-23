62f272545668fd26c1799b4894e6393e72b46cf2
Marcel Moreno <marcelclmoreno@gmail.com>
2014-03-23 10:59:13 -0300

    Script integration and fixes;

---
+++ Content/Scripts/scripts.js
@@ -0,0 +1,57 @@
+/*
+	polling
+*/
+function pollForImportedOrders() {
+    setTimeout(function() {
+        $.ajax({
+            url: "",
+            data : { },
+            type: "POST",
+            success: function(data) {
+            },
+            complete: function() {
+            },
+            timeout: 10000
+        })
+    }, 1000);
+}
+
+/* Utils */
+
+/* 
+	usage: $(selector).hasExtension(['.yolo']); 
+*/
+$.fn.hasExtension = function(exts) {
+    return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test($(this).val());
+}
+
+/* 
+	usage: $(selector).resetFormElement(); 
+*/
+$.fn.resetFormElement = function(element) {
+    return element.replaceWith(element.val("").clone(true));
+}
+
+/* 
+	usage: var json = toJSON(data); 
+*/
+function toJSON(obj) {
+    var json = JSON.stringify(obj);
+	json = JSON.parse(json);
+    return json;
+}
+
+/* 
+	if IE  (appliable to other browsers) 
+	this is a :last-child selector hack (appliable to other selectors/features)
+	God bless javascript;
+*/
+if($.browser.msie){
+	if($.browser.version == 7.0 || $.browser.version == 8.0) {
+		$('body *').each(function() {
+			if($(this).is(':last-child')) {
+				$(this).addClass('last-child');
+			}	
+		});
+	}
+}
