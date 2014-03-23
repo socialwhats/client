b2d6186286c9935e7a27b8f91dbdced846e571f3
Marcel Moreno <marcelclmoreno@gmail.com>
2014-03-23 12:23:49 -0300

    Script integration, callback implementation, and fixes

--- Content/Scripts/scripts.js
+++ Content/Scripts/scripts.js
@@ -41,6 +41,21 @@ function toJSON(obj) {
     return json;
 }
 
+
+/*
+	usage: var value = getQueryVariable(urlIdentifier);
+*/
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 /* 
 	if IE  (appliable to other browsers) 
 	this is a :last-child selector hack (appliable to other selectors/features)
